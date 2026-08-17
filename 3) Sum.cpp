c55149770ce0d44f2012ce97a9774cf80e3480ef
Sudip Sarkar <sarkarsudip2312@gmail.com>
2026-08-17 14:30:40 +0600

    Add files via upload

---
+++ 3) Sum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a, b, sum;
+    
+    cout<<"Enter Your 1st Number: ";
+    cin>>a;
+    
+    cout<<"Enter Your 2nd Number: ";
+    cin>>b;
+    
+    sum=a+b;
+    
+    cout<<"Your result is: " <<sum;
+    
+    return 0;
+}
