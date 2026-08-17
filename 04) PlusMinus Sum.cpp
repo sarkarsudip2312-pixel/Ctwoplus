7ea0a9ccbdc32e90aceaa6517b96c5dda9e71e83
Sudip Sarkar <sarkarsudip2312@gmail.com>
2026-08-18 01:46:30 +0600

    Add files via upload

---
+++ 04) PlusMinus Sum.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+    
+    int a,b,c,sum;
+    
+    cout<<"Enter Your 1st Number: ";
+    cin>>a;
+    
+    cout<<"Enter Your 2nd Number: ";
+    cin>>b;
+    
+    cout<<"Enter Your 3nd Number: ";
+    cin>>c;
+    
+    sum= a+b-c;
+    
+    cout<<"The result: "<<sum;
+    return 0;
+}
