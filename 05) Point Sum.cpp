d721062c0c363ffe9af0e954c762968a26acad8a
Sudip Sarkar <sarkarsudip2312@gmail.com>
2026-08-18 22:44:38 +0600

    Add files via upload

---
+++ 05) Point Sum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+float a,b,sum;
+
+cout<<"Enter your 1st number: ";
+cin>>a;
+
+cout<<"Enter your 2nd number: ";
+cin>>b;
+
+sum=a+b;
+
+cout<<"Your result:"<<sum;
+
+    return 0;
+}
