0ce493e50aa4573bcd3daa48bc2a6c4691c14099
Sudip Sarkar <sarkarsudip2312@gmail.com>
2026-08-20 12:37:06 +0600

    Add files via upload

---
+++ 06) Arithmetic Operators.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int a,b;
+    float x,y;
+    
+    cout<<"Enter 1st Number: ";
+    cin>>a,x;
+    
+    cout<<"Enter 2nd Number: ";
+    cin>>b,y;
+    
+    cout<<"Plus: "<<a+b<<endl;
+    cout<<"Minus: "<<a-b<<endl;
+    cout<<"Gun: "<<a*b<<endl;
+    cout<<"Vag: "<<x/y<<endl;
+    return 0;
+}
