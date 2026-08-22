5a51344aca00327e3571be93341f280932c6f826
Sudip Sarkar <sarkarsudip2312@gmail.com>
2026-08-23 01:47:12 +0600

    Add files via upload

---
+++ 09) Float.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main (){
+    float a,b,x;
+    
+    cout<<"Enter Your HSE GPA: ";
+    cin>>a;
+    
+    cout<<"Enter Your SSC GPA: ";
+    cin>>b;
+    
+    x=a+b;
+    
+    cout<<"Your Total GPA: "<<x;
+    
+    return 0;
+}
