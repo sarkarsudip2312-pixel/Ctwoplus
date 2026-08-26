da76b051eefc0f5bba1d0810779b9e0fafd084f6
Sudip Sarkar <sarkarsudip2312@gmail.com>
2026-08-27 01:15:04 +0600

    Add files via upload

---
+++ 12) While Loop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main () {
+     
+     int s=1;
+     
+     
+     while(s<=6){
+         
+         cout<<s<<endl;
+         
+         s++;
+     }
+    
+    return 0;
+}
